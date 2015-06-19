7a48e47a6f432632f6e11a51472ea6946fa0cd3d
Kotstullen <kaesegulasch@googlemail.com>
2015-06-19 21:52:09 +0200

    Update BewerbungsProgramm.java

--- BewerbungsProgramm.java
+++ BewerbungsProgramm.java
@@ -1,5 +1,8 @@
+// Däfinirt die Classe
 public class BewerbungsProgramm{
+  // Standad-Methode, stratet hier
   public static void main(String[] args){
+    // Gibt sachen in der konsole aus
     System.out.println("Kot.");
     System.out.println("Jeder isst gern Kot,\ndenn KOT IST LECKA!");
   }
