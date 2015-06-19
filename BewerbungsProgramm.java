010d3c0717d34c5ca0493db7b229099ea7b97a34
Kotstullen <kaesegulasch@googlemail.com>
2015-06-19 21:31:03 +0200

    Create BewerbungsProgramm.java

---
+++ BewerbungsProgramm.java
@@ -0,0 +1,6 @@
+public class BewerbungsProgramm{
+  public static void main(String[] args){
+    System.out.println("Kot.");
+    System.out.println("Jeder isst gern Kot,\ndenn KOT IST LECKA!");
+  }
+}
